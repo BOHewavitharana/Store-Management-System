b3b169aebd108b47cb44cc0969d9ccc84df85231
B.O.Hewavitharana <buddhimahewa@gmail.com>
2021-12-22 17:16:44 +0530

    Create BatchHistory.Designer.cs

---
+++ StoreMS/StoreMS/BatchHistory.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace StoreMS
+{
+    partial class BatchHistory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.backButton = new System.Windows.Forms.Button();
+            this.productLabel = new System.Windows.Forms.Label();
+            this.filterLabel = new System.Windows.Forms.Label();
+            this.supplierLabel = new System.Windows.Forms.Label();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.productComboBox = new System.Windows.Forms.ComboBox();
+            this.companyComboBox = new System.Windows.Forms.ComboBox();
+            this.BatchDataGridView = new System.Windows.Forms.DataGridView();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.BatchDataGridView)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.BackColor = System.Drawing.Color.Transparent;
+            this.backButton.BackgroundImage = global::StoreMS.Properties.Resources.arrows_11__Converted_;
+            this.backButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.backButton.FlatAppearance.BorderSize = 0;
+            this.backButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.backButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.backButton.ForeColor = System.Drawing.Color.White;
+            this.backButton.Location = new System.Drawing.Point(11, 11);
+            this.backButton.Margin = new System.Windows.Forms.Padding(2);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(33, 27);
+            this.backButton.TabIndex = 0;
+            this.backButton.UseVisualStyleBackColor = false;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // productLabel
+            // 
+            this.productLabel.AutoSize = true;
+            this.productLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.productLabel.ForeColor = System.Drawing.Color.Gray;
+            this.productLabel.Location = new System.Drawing.Point(22, 90);
+            this.productLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.productLabel.Name = "productLabel";
+            this.productLabel.Size = new System.Drawing.Size(44, 13);
+            this.productLabel.TabIndex = 1;
+            this.productLabel.Text = "Product";
+            // 
+            // filterLabel
+            // 
+            this.filterLabel.AutoSize = true;
+            this.filterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filterLabel.ForeColor = System.Drawing.Color.Gray;
+            this.filterLabel.Location = new System.Drawing.Point(22, 167);
+            this.filterLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.filterLabel.Name = "filterLabel";
+            this.filterLabel.Size = new System.Drawing.Size(99, 15);
+            this.filterLabel.TabIndex = 2;
+            this.filterLabel.Text = "Filter By Supplier";
+            this.filterLabel.Click += new System.EventHandler(this.filterLabel_Click);
+            // 
+            // supplierLabel
+            // 
+            this.supplierLabel.AutoSize = true;
+            this.supplierLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.supplierLabel.ForeColor = System.Drawing.Color.Gray;
+            this.supplierLabel.Location = new System.Drawing.Point(26, 191);
+            this.supplierLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.supplierLabel.Name = "supplierLabel";
+            this.supplierLabel.Size = new System.Drawing.Size(45, 13);
+            this.supplierLabel.TabIndex = 3;
+            this.supplierLabel.Text = "Supplier";
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categoryLabel.ForeColor = System.Drawing.Color.Gray;
+            this.categoryLabel.Location = new System.Drawing.Point(22, 42);
+            this.categoryLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(49, 13);
+            this.categoryLabel.TabIndex = 5;
+            this.categoryLabel.Text = "Category";
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.ForeColor = System.Drawing.Color.Gray;
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Location = new System.Drawing.Point(24, 57);
+            this.categoryComboBox.Margin = new System.Windows.Forms.Padding(2);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(213, 21);
+            this.categoryComboBox.TabIndex = 6;
+            this.categoryComboBox.Text = " ";
+            this.categoryComboBox.SelectionChangeCommitted += new System.EventHandler(this.categoryComboBox_SelectionChangeCommitted);
+            // 
+            // productComboBox
+            // 
+            this.productComboBox.ForeColor = System.Drawing.Color.Gray;
+            this.productComboBox.FormattingEnabled = true;
+            this.productComboBox.Location = new System.Drawing.Point(25, 105);
+            this.productComboBox.Margin = new System.Windows.Forms.Padding(2);
+            this.productComboBox.Name = "productComboBox";
+            this.productComboBox.Size = new System.Drawing.Size(212, 21);
+            this.productComboBox.TabIndex = 7;
+            this.productComboBox.SelectedIndexChanged += new System.EventHandler(this.productComboBox_SelectedIndexChanged);
+            // 
+            // companyComboBox
+            // 
+            this.companyComboBox.ForeColor = System.Drawing.Color.Gray;
+            this.companyComboBox.FormattingEnabled = true;
+            this.companyComboBox.Location = new System.Drawing.Point(24, 206);
+            this.companyComboBox.Margin = new System.Windows.Forms.Padding(2);
+            this.companyComboBox.Name = "companyComboBox";
+            this.companyComboBox.Size = new System.Drawing.Size(213, 21);
+            this.companyComboBox.TabIndex = 8;
+            this.companyComboBox.SelectedIndexChanged += new System.EventHandler(this.companyComboBox_SelectedIndexChanged);
+            this.companyComboBox.Click += new System.EventHandler(this.companyComboBox_Click);
+            // 
+            // BatchDataGridView
+            // 
+            this.BatchDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.BatchDataGridView.BackgroundColor = System.Drawing.Color.White;
+            this.BatchDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.BatchDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Gray;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.MediumSlateBlue;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.BatchDataGridView.DefaultCellStyle = dataGridViewCellStyle1;
+            this.BatchDataGridView.GridColor = System.Drawing.Color.Silver;
+            this.BatchDataGridView.Location = new System.Drawing.Point(280, 27);
+            this.BatchDataGridView.Margin = new System.Windows.Forms.Padding(2);
+            this.BatchDataGridView.Name = "BatchDataGridView";
+            this.BatchDataGridView.RowHeadersWidth = 51;
+            this.BatchDataGridView.RowTemplate.Height = 24;
+            this.BatchDataGridView.Size = new System.Drawing.Size(459, 424);
+            this.BatchDataGridView.TabIndex = 10;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(29, 269);
+            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(7);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 11;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.GhostWhite;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.categoryLabel);
+            this.panel1.Controls.Add(this.filterLabel);
+            this.panel1.Controls.Add(this.categoryComboBox);
+            this.panel1.Controls.Add(this.supplierLabel);
+            this.panel1.Controls.Add(this.companyComboBox);
+            this.panel1.Controls.Add(this.productComboBox);
+            this.panel1.Controls.Add(this.BatchDataGridView);
+            this.panel1.Controls.Add(this.productLabel);
+            this.panel1.Controls.Add(this.monthCalendar1);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(96, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(770, 475);
+            this.panel1.TabIndex = 12;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(9, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(260, 2);
+            this.label3.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(21, 247);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 15);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Filter By Date";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(22, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Please Select";
+            // 
+            // BatchHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::StoreMS.Properties.Resources.WhatsApp_Image_2021_09_30_at_10_16_33_PM;
+            this.ClientSize = new System.Drawing.Size(956, 517);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "BatchHistory";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BatchHistory";
+            this.Load += new System.EventHandler(this.BatchHistory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BatchDataGridView)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Label productLabel;
+        private System.Windows.Forms.Label filterLabel;
+        private System.Windows.Forms.Label supplierLabel;
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.ComboBox productComboBox;
+        private System.Windows.Forms.ComboBox companyComboBox;
+        private System.Windows.Forms.DataGridView BatchDataGridView;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
